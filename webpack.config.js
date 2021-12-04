fb09fde197ece0ffcaba7c84ef2d2754522f52df
lzn01 <408135242@qq.com>
2021-12-04 11:05:42 +0800

    添加webpack的 externals

--- webpack.config.js
+++ webpack.config.js
@@ -40,5 +40,21 @@ module.exports = {
             title: "PiggyUI",
             template: "index.html"
         })
-    ]
+    ],
+
+    // 8.1 配置external 排除react
+    externals: {
+        react: {
+            commonjs: "react",
+            commonjs2: "react",
+            amd: "react",
+            root: "React"
+        },
+        "react-dom": {
+            commonjs: "react-dom",
+            commonjs2: "react-dom",
+            amd: "react-dom",
+            root: "ReactDOM"
+        }
+    }
 };
