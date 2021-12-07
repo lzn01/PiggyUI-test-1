a63581b2642dfd783b12fff901aab94c71a0d0b9
lzn01 <408135242@qq.com>
2021-12-07 10:25:55 +0800

    icon 组件 className可传入

--- lib/index.tsx
+++ lib/index.tsx
@@ -2,4 +2,4 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import Icon from "../src/components/icon";
 
-ReactDOM.render(<Icon name={"wechat"}/>, document.querySelector('#root'));
+ReactDOM.render(<Icon name={"wechat"} className={'xxx'}/>, document.querySelector('#root'));
--- src/components/icon/index.tsx
+++ src/components/icon/index.tsx
@@ -2,18 +2,27 @@ import * as React from "react";
 import {FC} from "react";
 import "./methods/importAll";
 import "./index.scss";
+import classes from "../../utils/classes";
 
-interface IconProps {
+interface IconProps extends React.SVGAttributes<SVGElement> {
     name: string;
 }
 
-const Icon: FC<IconProps> = (props) => {
-    const {name} = props;
-    return (
-        <svg className={"piggyui-icon"}>
-            <use xlinkHref={`#${name}`}/>
-        </svg>
-    );
-};
+const componentName = "icon";
+
+const Icon: FC<IconProps> =
+    ({
+         className,
+         name,
+         ...restProps
+     }) => {
+        return (
+            <svg className={classes(componentName, "", [className])}
+                 {...restProps}
+            >
+                <use xlinkHref={`#${name}`}/>
+            </svg>
+        );
+    };
 
 export default Icon;
