b96b68c40e4c7090377518bae032efdcd88ddcfd
lzn01 <408135242@qq.com>
2021-12-07 16:32:09 +0800

    图标库新增

---
+++ src/components/icon/example/demo2/index.tsx
@@ -0,0 +1,10 @@
+import Icon from "../../index";
+import * as React from "react";
+
+const IconDemo2 = () => {
+    return (
+        <Icon name={"twitter"}/>
+    );
+};
+
+export default IconDemo2;
--- src/components/icon/example/index.tsx
+++ src/components/icon/example/index.tsx
@@ -1,10 +1,12 @@
 import * as React from "react";
 import IconDemo1 from "./demo1";
+import IconDemo2 from "./demo2";
 
 const IconExample = () => {
     return (
         <>
             <IconDemo1/>
+            <IconDemo2/>
         </>
     );
 };
