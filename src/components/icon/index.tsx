ed19f853d0a5df0ab9870c9ab1a7b50996168258
lzn01 <408135242@qq.com>
2021-12-08 09:49:48 +0800

    icon 样式修改

--- src/components/icon/index.tsx
+++ src/components/icon/index.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import {FC} from "react";
+import type {FC} from "react";
 import "./methods/importAll";
 import "./index.scss";
 import classes from "../../utils/classes";
