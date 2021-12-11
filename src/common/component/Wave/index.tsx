b0293b0313d2b5f0cd862029cb39ebccfb28c374
lzn01 <408135242@qq.com>
2021-12-11 16:59:29 +0800

    wave组件重构

--- src/common/component/Wave/index.tsx
+++ src/common/component/Wave/index.tsx
@@ -1,48 +1,39 @@
 import * as React from "react";
 import "./index.scss";
 import type {FC} from "react";
-import {useCallback, useEffect} from "react";
+import {useCallback, useEffect, useRef} from "react";
 import classes from "../../methods/classes";
 
-interface WaveProps {
-    closeWave?: boolean;
-}
+const Wave: FC = ({children}) => {
+    const animating = useRef<boolean | null>(null);
+    const originClassName = useRef<string>("");
 
-let animating: boolean;
-let originClassName: string;
+    const animationToEnd = useCallback((node: HTMLElement) => {
+        animating.current = false;
+        node.className = classes("", [originClassName]);
+        node.removeEventListener("animationend", () => animationToEnd(node));
+    }, []);
 
-const Wave: FC<WaveProps> =
-    ({
-         children,
-         closeWave
-     }) => {
+    const animationOnStart = useCallback((node: HTMLElement) => {
+        if (animating.current) return;
+        animating.current = true;
+        originClassName.current = node.className;
+        node.className = classes("", [originClassName, "pui-wave-animation-animating"]);
+        node.addEventListener("animationend", () => animationToEnd(node));
+    }, [animationToEnd]);
 
-        const animationToEnd = useCallback((node: HTMLElement) => {
-            animating = false;
-            node.className = classes("", [originClassName]);
+    useEffect(() => {
+        const node = (children as any).ref.current;
+        node.addEventListener("click", () => animationOnStart(node));
+        return () => {
+            node.removeEventListener("click", () => animationOnStart(node));
             node.removeEventListener("animationend", () => animationToEnd(node));
-        }, []);
+        };
+    }, [animationOnStart, animationToEnd, children]);
 
-        const animationOnStart = useCallback((node: HTMLElement) => {
-            if (animating || closeWave) return;
-            animating = true;
-            originClassName = node.className;
-            node.className = classes("", [originClassName, "pui-wave-animation-animating"]);
-            node.addEventListener("animationend", () => animationToEnd(node));
-        }, [animationToEnd, closeWave]);
-
-        useEffect(() => {
-            const node = (children as any).ref.current;
-            node?.addEventListener("click", () => animationOnStart(node));
-            return () => {
-                node?.removeEventListener("click", () => animationOnStart(node));
-                node?.removeEventListener("animationend", () => animationToEnd(node));
-            };
-        }, [animationOnStart, animationToEnd, children]);
-
-        return (
-            <>{children}</>
-        );
-    };
+    return (
+        <>{children}</>
+    );
+};
 
 export default Wave;
