7b8e37003d101352b0143b6b88acd8cf7db74312
lzn01 <408135242@qq.com>
2021-12-11 11:10:04 +0800

    添加ButtonGroup组件

---
+++ src/components/button/buttonGroup/index.tsx
@@ -0,0 +1,22 @@
+import * as React from "react";
+import {FC, HTMLAttributes} from "react";
+import "./index.scss";
+import classes from "../../../common/methods/classes";
+
+const componentName = "button-group";
+
+const ButtonGroup: FC<HTMLAttributes<HTMLElement>> =
+    ({
+         children,
+         className,
+         ...rest
+     }) => {
+        return (
+            <div className={classes(componentName, "", [className])}
+                 {...rest}
+            >
+                {children}
+            </div>
+        );
+    };
+export default ButtonGroup;
--- src/examples/button/demo1/index.tsx
+++ src/examples/button/demo1/index.tsx
@@ -1,32 +1,33 @@
 import * as React from "react";
 import Button from "../../../components/button";
+import ButtonGroup from "../../../components/button/buttonGroup";
 
 const ButtonDemo1 = () => {
     return (
         <>
-            <div style={{height:'50px',borderColor:"red",borderStyle:'solid',backgroundColor:'#e3ecd9'}}>
+            <div style={{height: "50px", borderColor: "red", borderStyle: "solid", backgroundColor: "#e3ecd9"}}>
                 <Button
                     disabled={false}
                     ghost={true}
-                    kind={'success'}
-                    size={'small'}
-                    icon={'wechat'}
+                    kind={"success"}
+                    size={"small"}
+                    icon={"wechat"}
                 >
                     小小
                 </Button>
                 <Button loading kind={"primary"}>中中</Button>
-                <Button size={"large"} icon={'twitter'} kind={'primary'} disabled position={"right"}>大大</Button>
+                <Button size={"large"} icon={"twitter"} kind={"primary"} disabled position={"right"}>大大</Button>
 
-                <Button kind={'success'}>成功</Button>
-                <Button kind={'warning'}>警告</Button>
+                <Button kind={"success"}>成功</Button>
+                <Button kind={"warning"}>警告</Button>
                 {/*<Button kind={'dashed'}>失败</Button>*/}
                 <Button>默认</Button>
                 <Button ghost>鬼鬼</Button>
-                <Button kind={'danger'}>大的</Button>
+                <Button kind={"danger"}>大的</Button>
             </div>
 
-            <div style={{height:'50px',borderColor:"red",borderStyle:'solid',backgroundColor:'#e3ecd9'}}>
-                <Button kind={"primary"} className={'success'}>按钮</Button>
+            <div style={{height: "50px", borderColor: "red", borderStyle: "solid", backgroundColor: "#e3ecd9"}}>
+                <Button kind={"primary"} className={"success"}>按钮</Button>
                 <Button kind={"primary"} ghost>按钮</Button>
                 <Button kind={"success"}>按钮</Button>
                 <Button kind={"success"} ghost>按钮</Button>
@@ -39,7 +40,14 @@ const ButtonDemo1 = () => {
                 <Button kind={"default"}>按钮</Button>
                 <Button kind={"default"} ghost>按钮</Button>
             </div>
-
+            <ButtonGroup>
+                <Button kind={"danger"}>按钮</Button>
+                <Button kind={"danger"}>按钮</Button>
+                <Button kind={"dashed"}>按钮</Button>
+                <Button kind={"dashed"} >按钮</Button>
+                <Button kind={"default"}>按钮</Button>
+                <Button kind={"default"} >按钮</Button>
+            </ButtonGroup>
         </>
     );
 };
