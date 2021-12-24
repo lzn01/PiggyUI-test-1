695a305be14cb13ddd2fbb2d0215882366f3b505
lzn01 <408135242@qq.com>
2021-12-24 09:29:54 +0800

    完成Row组件

---
+++ src/components/grid/index/Row.tsx
@@ -0,0 +1,37 @@
+import * as React from "react";
+import type {FC} from "react";
+import "../index.scss";
+import classes from "../../../common/methods/classes";
+import type {GridProps} from "../data";
+import type {ColProps} from "./Col";
+
+interface RowProps extends GridProps {
+}
+
+const componentName = "col";
+
+const Row: FC<RowProps> =
+    ({
+         children,
+         className,
+         gutter,
+         style,
+         ...rest
+     }) => {
+        return (
+            <div
+                className={classes(componentName, "", [className])}
+                style={{
+                    paddingLeft: `${gutter ? 0 : -gutter! / 2}px`,
+                    paddingRight: `${gutter ? 0 : -gutter! / 2}px`,
+                    ...style
+                }}
+                {...rest}
+            >
+                {React.Children.map(children, child => (
+                    React.cloneElement(child as React.ReactElement<ColProps>, {gutter})
+                ))}
+            </div>
+        );
+    };
+export default Row;
