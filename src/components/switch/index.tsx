5ef15627b9d80651c742879e3cfefeab718559af
lzn01 <408135242@qq.com>
2021-12-29 10:17:46 +0800

    初步完成switch组件

--- src/components/switch/index.tsx
+++ src/components/switch/index.tsx
@@ -1,9 +1,54 @@
 import * as React from "react";
 import "./index.scss";
+import classes from "../../common/methods/classes";
+import type {FC} from "react";
+import {useState} from "react";
 
-const Switch = () => {
-    return (
-        <div/>
-    );
-};
+interface SwitchProps {
+    checked?: boolean;
+    className?: string;
+    defaultChecked?: boolean;
+    disabled?: boolean;
+    onChange?: (checked: boolean, e: React.MouseEvent) => any;
+    size?: "small" | "default";
+    style?: React.CSSProperties;
+}
+
+const componentName = "switch";
+
+const Switch: FC<SwitchProps> =
+    ({
+         checked,
+         className,
+         defaultChecked,
+         disabled,
+         onChange,
+         size,
+         style
+     }) => {
+        const [derivedChecked, setDerivedChecked] = useState(false);
+
+        const clickHandler: React.MouseEventHandler = (e) => {
+            if (disabled) return;
+            if (onChange) {
+                onChange(!derivedChecked, e);
+            }
+            setDerivedChecked(!derivedChecked);
+        };
+
+        return (
+            <div
+                className={classes(
+                    componentName,
+                    "",
+                    [className, size],
+                    {checked: derivedChecked, disabled})
+                }
+                onClick={clickHandler}
+                style={style}
+            >
+                <span className={classes(componentName, "core")}/>
+            </div>
+        );
+    };
 export default Switch;
