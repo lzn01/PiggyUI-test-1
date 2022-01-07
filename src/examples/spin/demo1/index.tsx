211a9aff8d745e83f66da4e30021f352d7856c94
lzn01 <408135242@qq.com>
2022-01-07 09:59:49 +0800

    添加spin例子

--- src/examples/spin/demo1/index.tsx
+++ src/examples/spin/demo1/index.tsx
@@ -1,11 +1,36 @@
 import * as React from "react";
 import Spin from "../../../components/spin";
+import {useState} from "react";
+import Button from "../../../components/button";
+import "./index.scss";
 
 const SpinDemo1 = () => {
+    const [spinning, setSpinning] = useState(false);
     return (
         <>
             <div style={{width: "500px", height: "500px"}}>
                 <Spin/>
+                <Spin tip={"loading"}/>
+                {/*<Button onClick={() => setSpinning(!spinning)}>按钮</Button>*/}
+                <Spin spinning={spinning}/>
+            </div>
+            <div className={"container"}>
+                <Spin
+                    spinning={spinning}
+                    tip="loading..."
+                    size={30}
+                >
+                    <div style={{padding: "20px"}}>
+                        Further details about the context of this element.
+                    </div>
+                </Spin>
+                <Button
+                    onClick={() => setSpinning(!spinning)}
+                    kind="primary"
+                    style={{marginTop: "20px"}}
+                >
+                    Toggle
+                </Button>
             </div>
         </>
     );
