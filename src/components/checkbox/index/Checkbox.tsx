2306a3b945809b7c99fe264e5e8d663a5cf78cc0
lzn01 <408135242@qq.com>
2022-01-13 10:22:46 +0800

    checkbox组件逻辑实现

---
+++ src/components/checkbox/index/Checkbox.tsx
@@ -0,0 +1,57 @@
+import * as React from "react";
+import "../index.scss";
+import type {FC, MouseEvent} from "react";
+import {useEffect, useState} from "react";
+import classes from "../../../common/methods/classes";
+
+const componentName = "checkbox";
+
+interface CheckboxProps {
+    checked?: boolean; // 选中状态
+    defaultChecked?: boolean; // 默认选中状态
+    disabled?: boolean; // 禁用状态
+    onChange?: (checked: boolean, e: MouseEvent) => void;
+}
+
+const Checkbox: FC<CheckboxProps> =
+    ({
+         checked,
+         children,
+         defaultChecked = false,
+         disabled,
+         onChange
+     }) => {
+        const [checkboxState, setCheckboxState] = useState(defaultChecked);
+
+        const clickHandler = (e: MouseEvent) => {
+            if (disabled) return;
+            if (onChange) {
+                onChange(typeof checked === "boolean" ? checked : !checkboxState, e);
+            }
+            if (typeof checked !== "boolean") {
+                setCheckboxState(!checkboxState);
+            }
+        };
+
+        useEffect(() => {
+            if (typeof checked === "boolean") {
+                setCheckboxState(checked);
+            }
+        }, [checked]);
+
+        return (
+            <div
+                className={classes(componentName, "")}
+                onClick={clickHandler}
+            >
+                <div className={classes(componentName, "core", {
+                    disabled,
+                    checked: checkboxState
+                })}
+                >
+                    {children}
+                </div>
+            </div>
+        );
+    };
+export default Checkbox;
