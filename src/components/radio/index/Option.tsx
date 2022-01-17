b40f4a6adfcb437297dce8efe8e18731448c8994
lzn01 <408135242@qq.com>
2022-01-17 18:53:04 +0800

    option组件逻辑完成

--- src/components/radio/index/Option.tsx
+++ src/components/radio/index/Option.tsx
@@ -1,23 +1,49 @@
 import * as React from "react";
-import type {CSSProperties, FC} from "react";
+import type {FC, MouseEventHandler} from "react";
 import "../index.scss";
+import classes from "../../../common/methods/classes";
 
 interface OptionProps {
     checkedValue?: any;
-    className?: string;
     disabled?: boolean;
     onClick?: (checkedValue: any, e: React.MouseEvent) => any;
     radioStyle?: "radio" | "button";
-    style?: CSSProperties;
     value?: any;
     vertical?: boolean;
 }
 
 const componentName = "option";
 
-const Option: FC<OptionProps> = () => {
-    return (
-        <></>
-    );
-};
+const Option: FC<OptionProps> =
+    ({
+         checkedValue,
+         children,
+         disabled,
+         onClick,
+         radioStyle,
+         value,
+         vertical
+     }) => {
+        const clickHandler: MouseEventHandler = (e) => {
+            if (disabled || !onClick) {
+                return;
+            }
+            onClick(e, value);
+        };
+
+        return (
+            <label
+                className={classes(componentName, "", [radioStyle + "-style"], {
+                        checked: value === checkedValue,
+                        disabled,
+                        vertical
+                    }
+                )}
+                onClick={clickHandler}
+            >
+                {radioStyle === "radio" && <span className="label-dot"/>}
+                <div className="label-text">{children}</div>
+            </label>
+        );
+    };
 export default Option;
