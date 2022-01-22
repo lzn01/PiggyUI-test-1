4a9904503804fe55d288e57afffaa7a01f87acf7
lzn01 <408135242@qq.com>
2022-01-22 13:58:38 +0800

    新增走马灯组件

---
+++ src/components/carousel/index.tsx
@@ -0,0 +1,18 @@
+import * as React from "react";
+import "./index.scss";
+import type {FC} from "react";
+
+interface CarouselProps {
+
+}
+
+// const componentName = "carousel";
+
+
+const Carousel: FC<CarouselProps> =
+    ({}) => {
+    return (
+        <></>
+    );
+};
+export default Carousel;
