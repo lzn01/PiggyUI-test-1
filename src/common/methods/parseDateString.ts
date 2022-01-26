7f1f12b1ad2a96a45dbd0036c58616a19bacd474
lzn01 <408135242@qq.com>
2022-01-26 10:13:49 +0800

    parseDateString 方法

---
+++ src/common/methods/parseDateString.ts
@@ -0,0 +1,16 @@
+export const parseDateString = (str?: string) => {
+    if (str) {
+        const dateObject = new Date(str);
+        return {
+            year: dateObject.getFullYear(),
+            month: dateObject.getMonth() + 1,
+            date: dateObject.getDate()
+        };
+    }
+    const dateObject = new Date();
+    return {
+        year: dateObject.getFullYear(),
+        month: dateObject.getMonth() + 1,
+        date: dateObject.getDate()
+    };
+};
