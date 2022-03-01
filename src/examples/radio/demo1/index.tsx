72a719f1323adbd18f94e9bc3006d955bc51eadc
lzn01 <408135242@qq.com>
2022-03-01 19:07:52 +0800

    解决demo报错

--- src/examples/radio/demo1/index.tsx
+++ src/examples/radio/demo1/index.tsx
@@ -1,11 +1,11 @@
 import * as React from "react";
-import Radio from "../../../components/Radio/Radio";
-import Option from "../../../components/Radio/Option";
+import Radio from "../../../components/Radio";
 
+const {Option} = Radio;
 const RadioDemo1 = () => {
     return (
         <>
-            <Radio defaultValue="apple" radioStyle={'button'}>
+            <Radio defaultValue="apple" radioStyle={"button"}>
                 <Option value="apple">Apple</Option>
                 <Option value="orange">Orange</Option>
                 <Option value="pear">Pear</Option>
