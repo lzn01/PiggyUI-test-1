16c699c0dd888e08b50f9a738bfe1235afc7ac9f
lzn01 <408135242@qq.com>
2022-03-03 16:35:39 +0800

    Spin组件重构

---
+++ src/components/spin/Spin.tsx
@@ -0,0 +1,67 @@
+
+import * as React from "react";
+import "./index.scss";
+import type {CSSProperties, FC} from "react";
+import classes from "../../common/methods/classes";
+import Transition from "../../common/components/Transition/Transition";
+import Icon from "../Icon";
+import {useRef} from "react";
+
+interface SpinProps {
+    className?: string;
+    size?: number;
+    spinning?: boolean;
+    style?: CSSProperties;
+    tip?: string; // 自定义描述文案
+}
+
+const componentName = "spin";
+
+const Spin: FC<SpinProps> =
+    ({
+         children,
+         className,
+         size = 24,
+         spinning = true,
+         style,
+         tip
+     }) => {
+        const spinRef = useRef<HTMLDivElement | null>(null);
+
+        return (
+            <div
+                className={classes(componentName, "", [className], {"with-children": !!children})}
+                style={style}
+            >
+                <div className={classes(componentName, "mask", {active: !!children, spinning})}/>
+                <Transition
+                    visible={!!spinning}
+                    beforeEnter={{opacity: 0}}
+                    afterEnter={{opacity: 1}}
+                >
+                    <div
+                        className={classes(componentName, "container", {
+                                "with-tip": !!tip,
+                                "with-children": !!children
+                            }
+                        )}
+                        ref={spinRef}
+                    >
+                        <Icon
+                            className={classes(componentName, "icon")}
+                            name="loading"
+                            size={size}
+                        />
+                        {
+                            tip &&
+                            <span className={classes(componentName, "tip")}>
+                                {tip}
+                            </span>
+                        }
+                    </div>
+                </Transition>
+                {children}
+            </div>
+        );
+    };
+export default Spin;
