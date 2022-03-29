5139886b1b2b8b89eb624df389b63388ba3809c4
lzn01 <408135242@qq.com>
2022-03-29 11:08:41 +0800

    Add CarouselProps

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -1,19 +1,38 @@
 import * as React from 'react';
-import { Children, useEffect, useRef, useState } from 'react';
-import classes from '../../common/methods/classes';
+import { isNotUndefined } from '../../common/methods/is';
 import './styles/index.scss';
 import type { CSSProperties, FC } from 'react';
+import type { Settings } from 'react-slick';
 
-interface CarouselProps {
-    afterChange?: (current: number, from: number) => any; // 面板切换完成时的回调
-    beforeChange?: (from: number, to: number) => any; // 面板开始切换时的回调
-    className?: string;
-    dots?: boolean; // 是否显示面板指示点
-    duration?: number; // 自动播放间隔，设置为 0 时不自动播放，单位：秒
-    style?: CSSProperties;
+if (isNotUndefined(window)) {
+    const matchMediaPolyfill = (mediaQuery: string) => ({
+        media: mediaQuery,
+        matches: false,
+        addListener() {},
+        removeListener() {},
+    }) as unknown as MediaQueryList;
+    window.matchMedia = matchMedia || matchMediaPolyfill;
 }
 
-const componentName = 'Carousel';
+export type CarouselEffect = 'scrollx' | 'fade';                // 动画效果
+export type DotsPosition = 'top' | 'bottom' | 'left' | 'right'; // 面板指示点的四个显示位置
+
+export interface CarouselProps extends Omit<Settings, 'dots'> {
+    dots?: boolean;                                             // 是否显示面板指示点
+    dotsPosition?: DotsPosition;                                // 面板指示点的显示位置
+    effect?: CarouselEffect;                                    // 动画效果函数
+    style?: CSSProperties;                                      // 容器样式
+}
+
+export interface CarouselRef {
+    goTo: (slide: number, dontAnimate?: boolean) => void;       // 切换到指定面板
+    next: () => void;                                           // 切换到下一面板
+    prev: () => void;                                           // 切换到上一面板
+    autoPlay: boolean;                                          // 是否自动切换
+    innerSlider: any;
+}
+
+// const componentName = 'Carousel';
 
 const Carousel: FC<CarouselProps> =
     ({
@@ -22,64 +41,10 @@ const Carousel: FC<CarouselProps> =
          children,
          className,
          dots,
-         duration,
          style,
      }) => {
-        const containerRef = useRef<HTMLDivElement | null>(null);
-        const [current, setCurrent] = useState(1);
-        const [hasTransitionClassName, setHasTransitionClassName] = useState(true);
-
-        const cloneNode = () => {
-            const nodeList: HTMLElement[] = [];
-            const containerNode = containerRef.current;
-            containerNode?.childNodes.forEach(node => {
-                if (node.nodeType === 1) {
-                    const eleNode = node as HTMLElement;
-                    nodeList.push(eleNode);
-                }
-            });
-            containerNode?.append(nodeList[0].cloneNode(true));
-            containerNode?.prepend(nodeList[nodeList.length - 1].cloneNode(true));
-        };
-
-        const containerClassName = classes(componentName, 'container', {
-            'has-transition-class-name': hasTransitionClassName,
-        });
-
-        useEffect(() => {
-            cloneNode();
-        }, []);
-
         return (
-            <div
-                className={classes(componentName, '', [className])}
-                style={style}
-            >
-                <div
-                    className={containerClassName}
-                    ref={containerRef}
-                >
-                    {children}
-                </div>
-                {
-                    dots &&
-                    <div className={classes(componentName, 'dot-wrapper')}>
-                        {
-                            Children.map(children, (
-                                child, index) =>
-                                <span
-                                    className={classes(componentName, 'dot', {
-                                            active: current === index + 1,
-                                        },
-                                    )}
-                                    onClick={() => {
-                                    }}
-                                />,
-                            )
-                        }
-                    </div>
-                }
-            </div>
+            <></>
         );
     };
 export default Carousel;
