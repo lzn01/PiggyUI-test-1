6c68e87536840d8721a258f7963c599f6928f720
lzn01 <408135242@qq.com>
2022-04-19 20:10:08 +0800

    添加reset.scss

--- src/examples/Carousel/index.tsx
+++ src/examples/Carousel/index.tsx
@@ -5,11 +5,11 @@ import './index.scss';
 const CarouselExample = () => {
     return (
         <div className={'carousel'}>
-            <Carousel arrows dotsTimer className={'abc'}>
-                <div><h3 style={{ color: '#fff' }}>1</h3></div>
-                <div><h3 style={{ color: '#fff' }}>2</h3></div>
-                <div><h3 style={{ color: '#fff' }}>3</h3></div>
-                <div><h3 style={{ color: '#fff' }}>4</h3></div>
+            <Carousel arrows dotsTimer>
+                <div className={'abc'}><h3 style={{ color: '#fff' }}>1</h3></div>
+                <div className={'abc'}><h3 style={{ color: '#fff' }}>2</h3></div>
+                <div className={'abc'}><h3 style={{ color: '#fff' }}>3</h3></div>
+                <div className={'abc'}><h3 style={{ color: '#fff' }}>4</h3></div>
             </Carousel>
         </div>
     );
