47b748734329cec3e77a1b34ffd2d15b90f0ed05
lzn01 <408135242@qq.com>
2022-04-25 14:06:39 +0800

    Carousel 样式调整

--- src/examples/Carousel/index.tsx
+++ src/examples/Carousel/index.tsx
@@ -5,7 +5,7 @@ import './index.scss';
 const CarouselExample = () => {
     return (
         <div className={'carousel'}>
-            <Carousel arrows dotsPosition={'left'} autoplay dotsTimer>
+            <Carousel arrows dotsPosition={'left'} autoplay={false} >
                 <div className={'abc'}><h3 style={{ color: '#fff' }}>1</h3></div>
                 <div className={'abc'}><h3 style={{ color: '#fff' }}>2</h3></div>
                 <div className={'abc'}><h3 style={{ color: '#fff' }}>3</h3></div>
