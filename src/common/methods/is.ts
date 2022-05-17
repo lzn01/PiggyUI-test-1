eefabd396173ebd57efe0667180225537f10518b
lzn01 <408135242@qq.com>
2022-05-17 20:02:58 +0800

    isPercent isInseparable

--- src/common/methods/is.ts
+++ src/common/methods/is.ts
@@ -6,3 +6,6 @@ export const isNumber = (v: any) => typeof v === 'number';
 export const isObject = (v: any) => v && typeof v === 'object' && !isArray(v);
 export const isString = (v: any) => typeof v === 'string';
 export const isBoolean = (v: any) => typeof v === 'boolean';
+export const isPercent = n => typeof n === 'string' && /\d{1,3}%$/.test(n)
+export const isInseparable = val =>
+    Object(val) !== val || isFunc(val) || isDate(val) || isError(val) || isSet(val) || isMap(val) || isRegexp(val)
