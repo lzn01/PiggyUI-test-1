cf8a0515ed381e2a47a8238ca181a0840da0efe2
lzn01 <408135242@qq.com>
2022-05-19 18:48:47 +0800

    isLink 方法

--- src/common/methods/is.ts
+++ src/common/methods/is.ts
@@ -1,3 +1,5 @@
+import { isValidElement } from 'react';
+
 export const isArray = (v: any) => Array.isArray(v);
 export const isUndefined = (v: any) => v == null;
 export const isNotUndefined = (v: any) => v != null;
@@ -6,6 +8,16 @@ export const isNumber = (v: any) => typeof v === 'number';
 export const isObject = (v: any) => v && typeof v === 'object' && !isArray(v);
 export const isString = (v: any) => typeof v === 'string';
 export const isBoolean = (v: any) => typeof v === 'boolean';
-export const isPercent = n => typeof n === 'string' && /\d{1,3}%$/.test(n)
-export const isInseparable = val =>
-    Object(val) !== val || isFunc(val) || isDate(val) || isError(val) || isSet(val) || isMap(val) || isRegexp(val)
+export const isLink = (el: {} | null | undefined) => {
+    if (!isValidElement(el)) return false;
+    if (!el.type) return false;
+    if (el.type === 'a') return true;
+    return !!(el.props && el.props.to);
+
+};
+
+export const isEnterPress = (e: { keyCode: number; }) => e.keyCode === 13;
+
+export const isMacOS = () => /macintosh|mac os x/i.test(navigator.userAgent);
+
+export const isFirefox = () => navigator.userAgent.toLowerCase().indexOf('firefox') > -1;
