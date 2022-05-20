9207d0e34dede6636c85ef52daa4ee95a58def9e
lzn01 <408135242@qq.com>
2022-05-20 15:30:02 +0800

    Switch

--- src/components/Switch/Switch.tsx
+++ src/components/Switch/Switch.tsx
@@ -1,66 +1,94 @@
 import * as React from 'react';
-import { useEffect, useState } from 'react';
-import { isBoolean } from '../../common/methods/is';
+import * as PropTypes from 'prop-types';
+import './style';
 import { classes } from '../../common/methods/classes';
-import './styles/index.scss';
-import type { CSSProperties, FC } from 'react';
 
-type Size = 'small' | 'default';
-
-interface SwitchProps {
-    checked?: boolean; // 当前选中状态
-    className?: string;
-    defaultChecked?: boolean; // 默认选中状态
+export interface SwitchProps {
+    checked?: boolean;
+    defaultChecked?: boolean;
+    size?: 'small' | 'default';
+    onChange?: (checked: boolean, e: React.MouseEvent) => any;
     disabled?: boolean;
-    onChange?: (checked: boolean, e: React.MouseEvent) => void;
-    size?: Size;
-    style?: CSSProperties;
+    className?: string;
+    style?: React.CSSProperties;
+}
+
+export interface SwitchState {
+    derivedChecked: boolean;
 }
 
 const componentName = 'Switch';
 
-const Switch: FC<SwitchProps> =
-    ({
-         checked,
-         className,
-         defaultChecked = false,
-         disabled = false,
-         onChange,
-         size,
-         style,
-     }) => {
-        const [switchState, setSwitchState] = useState(defaultChecked); // 开关状态 true开 false关
+class Switch extends React.Component<SwitchProps, SwitchState> {
+    public static displayName = componentName;
 
-        const clickHandler: React.MouseEventHandler = (e) => {
-            if (disabled) return;
-            if (onChange) {
-                onChange(isBoolean(checked) ? !!checked : !switchState, e);
-            }
-            if (!isBoolean(checked)) {
-                setSwitchState(!switchState);
-            }
+    public static defaultProps = {
+        size: 'default',
+        defaultChecked: false,
+        disabled: false,
+    };
+
+    public static propTypes = {
+        defaultChecked: PropTypes.bool,
+        onChange: PropTypes.func,
+        size: PropTypes.oneOf(['small', 'default']),
+        disabled: PropTypes.bool,
+        className: PropTypes.string,
+        style: PropTypes.object,
+    };
+
+    public static getDerivedStateFromProps(
+        nextProps: SwitchProps,
+        prevState: SwitchState,
+    ) {
+        const { checked } = nextProps;
+        const { derivedChecked } = prevState;
+        if ('checked' in nextProps && checked !== derivedChecked) {
+            return { derivedChecked: checked };
+        }
+        return null;
+    }
+
+    constructor(props: SwitchProps) {
+        super(props);
+        this.state = {
+            derivedChecked: props.defaultChecked || false,
         };
+    }
 
-        useEffect(() => {
-            if (isBoolean(checked)) {
-                setSwitchState(!!checked);
-            }
-        }, [checked]);
+    public handleClick: React.MouseEventHandler = e => {
+        const { disabled, onChange } = this.props;
+        if (disabled) {
+            return;
+        }
+        const { derivedChecked } = this.state;
+        this.setState({
+            derivedChecked: !derivedChecked,
+        });
 
+        if (onChange) {
+            onChange(!derivedChecked, e);
+        }
+    };
+
+    public render() {
+        const cn = componentName;
+        const { size, disabled, style, className } = this.props;
+        const { derivedChecked } = this.state;
+        const switchClassName = classes(cn, '', [className, size], {
+            checked: derivedChecked,
+            disabled,
+        });
         return (
-            <div
-                className={classes(
-                    componentName,
-                    '',
-                    [className, size ?? 'default'],
-                    { checked: switchState, disabled },
-                )}
-                onClick={clickHandler}
+            <span
+                className={switchClassName}
+                onClick={this.handleClick}
                 style={style}
             >
-                <span className={classes(componentName, 'core')} />
-            </div>
+        <span className={classes(cn, 'core')} />
+      </span>
         );
-    };
+    }
+}
 
 export default Switch;
