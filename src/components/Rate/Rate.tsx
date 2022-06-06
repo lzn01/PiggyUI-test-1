a2655c680a4c3e23359ba6f66a66cfd1ab34c4e5
lzn01 <408135242@qq.com>
2022-06-06 18:50:04 +0800

    评分组件代码结构调整

--- src/components/Rate/Rate.tsx
+++ src/components/Rate/Rate.tsx
@@ -21,106 +21,107 @@ interface RateProps {
 
 const componentName = 'Rate';
 
-const Rate: FC<RateProps> =
-    ({
-         allowClear = false,
-         allowHalf = false,
-         className,
-         defaultValue = 0,
-         disabled = false,
-         onChange,
-         onHover,
-         style,
-         tips,
-         value,
-     }) => {
-        const [hoveredRateValue, setHoveredRateValue] = useState(0);
-        const [selectedRateValue, setSelectedRateValue] = useState(defaultValue);
+const Rate: FC<RateProps> = (
+    {
+        allowClear = false,
+        allowHalf = false,
+        className,
+        defaultValue = 0,
+        disabled = false,
+        onChange,
+        onHover,
+        style,
+        tips,
+        value,
+    },
+) => {
+    const [hoveredRateValue, setHoveredRateValue] = useState(0);
+    const [selectedRateValue, setSelectedRateValue] = useState(defaultValue);
 
-        const changeHandler = (param: number) => {
-            if (onChange) {
-                onChange(param);
-            }
-        };
-
-        const hoverHandler = (param: number) => {
-            if (onHover) {
-                onHover(param);
-            }
-        };
-
-        // 处理星星点击事件
-        const clickHandler = (index: number, position: string) => {
-            const newValue = (allowHalf && position === 'left' ? 0.5 : 1) + index;
-            const valueState = selectedRateValue === newValue;
-            if (disabled) return;
-            if (allowClear) {
-                changeHandler(valueState ? 0 : newValue);
-                setSelectedRateValue(valueState ? 0 : newValue);
-                valueState && setHoveredRateValue(0);
-            } else if (!valueState) {
-                changeHandler(newValue);
-                setSelectedRateValue(newValue);
-            }
-        };
-
-        // 处理容器内鼠标移动
-        const mouseEnterHandler = (index: number, position: string) => {
-            const newValue = (position === 'left' ? 0.5 : 1) + index;
-            if (disabled) return;
-            if (allowHalf && hoveredRateValue !== newValue) {
-                hoverHandler(newValue);
-                setHoveredRateValue(newValue);
-            }
-            if (!allowHalf && hoveredRateValue !== index + 1) {
-                hoverHandler(index + 1);
-                setHoveredRateValue(index + 1);
-            }
-        };
-
-        // 处理容器外鼠标移动
-        const mouseLeaveHandler = () => {
-            hoverHandler(0);
-            setHoveredRateValue(0);
-        };
-
-        useEffect(() => {
-            if (!isUndefined(value)) {
-                setSelectedRateValue(value as number);
-            }
-        }, [value]);
-
-        return (
-            <div
-                className={classes(componentName, '', [className])}
-                style={style}
-            >
-                <ul
-                    className={classes(componentName, 'container')}
-                    onMouseLeave={mouseLeaveHandler}
-                >
-                    {
-                        Array
-                            .from({ length: 5 }, (v, k) => `item-${k}`)
-                            .map((_, index) =>
-                                <Star
-                                    key={_}
-                                    count={hoveredRateValue || selectedRateValue}
-                                    index={index}
-                                    onClick={clickHandler}
-                                    onMouseEnter={mouseEnterHandler}
-                                />,
-                            )
-                    }
-                </ul>
-                {
-                    tips &&
-                    <div className={classes(componentName, 'tips')}>
-                        {tips}
-                    </div>
-                }
-            </div>
-        );
+    const changeHandler = (param: number) => {
+        if (onChange) {
+            onChange(param);
+        }
     };
+
+    const hoverHandler = (param: number) => {
+        if (onHover) {
+            onHover(param);
+        }
+    };
+
+    // 处理星星点击事件
+    const clickHandler = (index: number, position: string) => {
+        const newValue = (allowHalf && position === 'left' ? 0.5 : 1) + index;
+        const valueState = selectedRateValue === newValue;
+        if (disabled) return;
+        if (allowClear) {
+            changeHandler(valueState ? 0 : newValue);
+            setSelectedRateValue(valueState ? 0 : newValue);
+            valueState && setHoveredRateValue(0);
+        } else if (!valueState) {
+            changeHandler(newValue);
+            setSelectedRateValue(newValue);
+        }
+    };
+
+    // 处理容器内鼠标移动
+    const mouseEnterHandler = (index: number, position: string) => {
+        const newValue = (position === 'left' ? 0.5 : 1) + index;
+        if (disabled) return;
+        if (allowHalf && hoveredRateValue !== newValue) {
+            hoverHandler(newValue);
+            setHoveredRateValue(newValue);
+        }
+        if (!allowHalf && hoveredRateValue !== index + 1) {
+            hoverHandler(index + 1);
+            setHoveredRateValue(index + 1);
+        }
+    };
+
+    // 处理容器外鼠标移动
+    const mouseLeaveHandler = () => {
+        hoverHandler(0);
+        setHoveredRateValue(0);
+    };
+
+    useEffect(() => {
+        if (!isUndefined(value)) {
+            setSelectedRateValue(value as number);
+        }
+    }, [value]);
+
+    return (
+        <div
+            className={classes(componentName, '', [className])}
+            style={style}
+        >
+            <ul
+                className={classes(componentName, 'container')}
+                onMouseLeave={mouseLeaveHandler}
+            >
+                {
+                    Array
+                        .from({ length: 5 }, (v, k) => `item-${k}`)
+                        .map((_, index) =>
+                            <Star
+                                key={_}
+                                count={hoveredRateValue || selectedRateValue}
+                                index={index}
+                                onClick={clickHandler}
+                                onMouseEnter={mouseEnterHandler}
+                            />,
+                        )
+                }
+            </ul>
+            {
+                tips &&
+                <div className={classes(componentName, 'tips')}>
+                    {tips}
+                </div>
+            }
+        </div>
+    );
+};
 
 export default Rate;
--- src/components/Rate/Star.tsx
+++ src/components/Rate/Star.tsx
@@ -15,55 +15,56 @@ interface StarProps {
 
 const componentName = 'Star';
 
-const Star: FC<StarProps> =
-    ({
-         count,
-         index,
-         onClick,
-         onMouseEnter,
-     }) => {
-        const iconNameHandler = () => {
-            const starValue = count - index;
-            if (starValue < 0.5) {
-                return 'empty-star';
-            }
-            if (starValue > 0.5) {
-                return 'full-star';
-            }
-            return 'half-star';
-        };
-
-        const mouseEnterHandler = (position: 'left' | 'right') => {
-            if (onMouseEnter) {
-                onMouseEnter(index, position);
-            }
-        };
-
-        const clickHandler = (position: 'left' | 'right') => {
-            if (onClick) {
-                onClick(index, position);
-            }
-        };
-
-        return (
-            <li className={classes(componentName, '')}>
-                <Icon
-                    name={iconNameHandler()}
-                    size={20}
-                    className="pui-rate-star-icon"
-                />
-                <div
-                    className={classes(componentName, 'filler', ['left'])}
-                    onMouseEnter={() => mouseEnterHandler('left')}
-                    onClick={() => clickHandler('left')}
-                />
-                <div
-                    className={classes(componentName, 'filler', ['right'])}
-                    onMouseEnter={() => mouseEnterHandler('right')}
-                    onClick={() => clickHandler('right')}
-                />
-            </li>
-        );
+const Star: FC<StarProps> = (
+    {
+        count,
+        index,
+        onClick,
+        onMouseEnter,
+    },
+) => {
+    const iconNameHandler = () => {
+        const starValue = count - index;
+        if (starValue < 0.5) {
+            return 'empty-star';
+        }
+        if (starValue > 0.5) {
+            return 'full-star';
+        }
+        return 'half-star';
     };
+
+    const mouseEnterHandler = (position: 'left' | 'right') => {
+        if (onMouseEnter) {
+            onMouseEnter(index, position);
+        }
+    };
+
+    const clickHandler = (position: 'left' | 'right') => {
+        if (onClick) {
+            onClick(index, position);
+        }
+    };
+
+    return (
+        <li className={classes(componentName, '')}>
+            <Icon
+                name={iconNameHandler()}
+                size={20}
+                className="pui-rate-star-icon"
+            />
+            <div
+                className={classes(componentName, 'filler', ['left'])}
+                onMouseEnter={() => mouseEnterHandler('left')}
+                onClick={() => clickHandler('left')}
+            />
+            <div
+                className={classes(componentName, 'filler', ['right'])}
+                onMouseEnter={() => mouseEnterHandler('right')}
+                onClick={() => clickHandler('right')}
+            />
+        </li>
+    );
+};
 
 export default Star;
