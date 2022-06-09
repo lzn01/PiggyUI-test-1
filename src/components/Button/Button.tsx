69c2047d2aeb51298682dc0ac13b46813d8b889d
lzn01 <408135242@qq.com>
2022-06-09 09:35:55 +0800

    Button 组件代码结构修改

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -28,51 +28,51 @@ const componentName = 'Button';
 // const twoCNCharRegex = /^[\u4e00-\u9fa5]{2}$/; // regex char 字符
 // const isTwoCNChar = twoCNCharRegex.test.bind(twoCNCharRegex); // test() 方法用于检测一个字符串是否匹配某个模式
 
-const Button: ButtonInterface =
-    ({
-         children,
-         className,
-         disabled,
-         ghost,
-         icon,
-         loading,
-         position,
-         size,
-         type,
-         ...rest
-     }: ButtonProps) => {
-        const buttonRef = useRef<HTMLButtonElement | null>(null);
-        const buttonClassName = classes(
-            componentName,
-            '',
-            [className, `p-${type ?? 'default'}`, `p-${position ?? 'left'}`, `p-${size ?? 'medium'}`],
-            { ghost, disabled },
-        );
+const Button: ButtonInterface = (
+    {
+        children,
+        className,
+        disabled,
+        ghost,
+        icon,
+        loading,
+        position,
+        size,
+        type,
+        ...rest
+    }: ButtonProps) => {
+    const buttonRef = useRef<HTMLButtonElement | null>(null);
+    const buttonClassName = classes(
+        componentName,
+        '',
+        [className, `p-${type ?? 'default'}`, `p-${position ?? 'left'}`, `p-${size ?? 'medium'}`],
+        { ghost, disabled },
+    );
 
-        const iconRenderHandler = () => {
-            const iconClassName = classes(componentName, 'icon', [size ?? 'medium'], { loading });
-            return loading
-                ? <Icon name={'wechat'} className={iconClassName} />
-                : icon && <Icon name={icon} className={iconClassName} />;
-        };
+    const iconRenderHandler = () => {
+        const iconClassName = classes(componentName, 'icon', [size ?? 'medium'], { loading });
+        return loading
+            ? <Icon name={'wechat'} className={iconClassName} />
+            : icon && <Icon name={icon} className={iconClassName} />;
+    };
 
-        return (
-            <Wave>
-                <button
-                    className={buttonClassName}
-                    disabled={disabled}
-                    ref={buttonRef}
-                    {...rest}
-                >
-                    {iconRenderHandler()}
-                    <span className={classes(componentName, 'text')}>
+    return (
+        <Wave>
+            <button
+                className={buttonClassName}
+                disabled={disabled}
+                ref={buttonRef}
+                {...rest}
+            >
+                {iconRenderHandler()}
+                <span className={classes(componentName, 'text')}>
                         {children}
                     </span>
-                    {!disabled && <span className={classes(componentName, 'ripple')} />}
-                </button>
-            </Wave>
-        );
-    };
+                {!disabled && <span className={classes(componentName, 'ripple')} />}
+            </button>
+        </Wave>
+    );
+};
 
 Button.Group = Group;
 export default Button;
